29ce927552f53ef8a3f9f7a01ba1569f70363cef
Nick Tran <nick.tran@ejbsys.com>
2021-11-30 13:01:15 -0500

    completes section 4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,61 @@
 class ArticlesController < ApplicationController
+    
+    before_action :set_article, only: [:show,:edit, :update, :destroy]
+    
+    
     def show
-        #byebug
-        @article = Article.find(params[:id])
+        
     end
 
     def index
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def edit
+  
+    end
+
+    def update
+        #@article =Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] =  "Article updated"
+            redirect_to (@article)
+        else
+            render 'edit'
+        end
+    end
+
+    def create
+        @article =Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article created successfully"
+            #render plain: @article.id
+            #redirect_to article_path(@article) or can do this
+            redirect_to (@article)
+        else
+            render 'new'
+        end
+    end
+
+    def destroy
+        #@article =Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
   end
+
+  
